49e50f8a391decb64443f656fd933c57fdcd1256
lieu18 <lieu18@up.edu>
2018-02-13 00:39:07 -0800

    Added Listeners for each widget Face now extends SurfaceView began onDraw method functionality

--- app/src/main/java/com/example/anthonylieu/hw02_facemaker/Face.java
+++ app/src/main/java/com/example/anthonylieu/hw02_facemaker/Face.java
@@ -1,5 +1,9 @@
 package com.example.anthonylieu.hw02_facemaker;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
 import android.graphics.Color;
 import android.graphics.Paint;
 
@@ -9,22 +13,44 @@ import java.util.Random;
  * Created by AnthonyLieu on 2/12/18.
  */
 
-public class Face {
+// https://learning.up.edu/moodle/pluginfile.php/635566/mod_resource/content/0/MyArtwork.java
+public class Face extends SurfaceView {
+
+    private Paint skinColor, eyeColor, hairColor;
+
+    private SurfaceView sv;
+
     private int skinRed, skinGreen, skinBlue;
     private int eyeRed, eyeGreen, eyeBlue;
     private int hairRed, hairGreen, hairBlue;
-    private Paint skinColor, eyeColor, hairColor;
+
     private int hairStyle;
 
-    Random gen = new Random();
+    private Random gen = new Random();
 
-    public Face(){
-        randomize();
+    public Face(Context context) {
+        super(context);
+        faceInit();
     }
 
+    public Face(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        faceInit();
+    }
+
+    public Face(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        faceInit();
+    }
+
+    public void faceInit() {
+        setWillNotDraw(false);
+        randomize();
+        sv = (SurfaceView) findViewById(R.id.surfaceView);
+    }
 
     /*
-     * method should ini
+     * randomize method to randomize the color of the skin, eyes, and hair
      */
     public void randomize() {
 
@@ -47,6 +73,15 @@ public class Face {
         hairColor.setColor(Color.rgb(hairRed, hairGreen, hairBlue));
 
         hairStyle = gen.nextInt(3) + 1;
+    }
+
+
+    public void onDraw(Canvas canvas) {
+
+        //canvas.drawRect(400f, 100f, 1400f, 900f, hairColor); // Flat Top Hair
+        //canvas.drawCircle(900f, 700f, 600f, hairColor); // Afro Hair
+
+        canvas.drawCircle(900f, 900f, 500f, skinColor);
     }
 }
 
--- app/src/main/java/com/example/anthonylieu/hw02_facemaker/MainActivity.java
+++ app/src/main/java/com/example/anthonylieu/hw02_facemaker/MainActivity.java
@@ -1,13 +1,97 @@
 package com.example.anthonylieu.hw02_facemaker;
 
+import android.provider.MediaStore;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import static com.example.anthonylieu.hw02_facemaker.R.id.seekBarRed;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] hairStyles =
+            {"Afro", "Flat Top", "Bowl Cut"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // https://learning.up.edu/moodle/pluginfile.php/633654/mod_resource/content/0/MainActivity.java
+        ArrayAdapter<String> spinAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, hairStyles);
+        Spinner spinnerHairStyle =
+                (Spinner) findViewById(R.id.spinnerHairStyle);
+        spinnerHairStyle.setAdapter(spinAdapter);
+
+
+        SeekBar seekBarRed =
+                (SeekBar) findViewById(R.id.seekBarRed);
+        seekBarRed.setOnSeekBarChangeListener(new seekBarListener());
+        SeekBar seekBarGreen =
+                (SeekBar) findViewById(R.id.seekBarGreen);
+        seekBarGreen.setOnSeekBarChangeListener(new seekBarListener());
+        SeekBar seekBarBlue =
+                (SeekBar) findViewById(R.id.seekBarBlue);
+        seekBarBlue.setOnSeekBarChangeListener(new seekBarListener());
+
+        // https://stackoverflow.com/questions/18536195/android-oncheckedchanged-for-radiogroup
+
+        RadioGroup radioGroup =
+                (RadioGroup) findViewById(R.id.radioGroup);
+            RadioButton radioButtonHair =
+                    (RadioButton) findViewById(R.id.radioButtonHair);
+            RadioButton radioButtonEyes =
+                    (RadioButton) findViewById(R.id.radioButtonEyes);
+            RadioButton radioButtonSkin =
+                    (RadioButton) findViewById(R.id.radioButtonSkin);
+            radioGroup.setOnCheckedChangeListener(new radioGroupListener());
+
+    }
+
+    private class seekBarListener implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (seekBar.getId() == R.id.seekBarRed) {
+                TextView textViewRed =
+                        (TextView) findViewById(R.id.textViewRed);
+                textViewRed.setText("Red: " + progress);
+            }
+            else if (seekBar.getId() == R.id.seekBarGreen) {
+                TextView textViewGreen =
+                        (TextView) findViewById(R.id.textViewGreen);
+                textViewGreen.setText("Green: " + progress);
+            }
+            else if (seekBar.getId() == R.id.seekBarBlue) {
+                TextView textViewBlue =
+                        (TextView) findViewById(R.id.textViewBlue);
+                textViewBlue.setText("Blue: " + progress);
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
+
+    private class radioGroupListener implements RadioGroup.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+        }
     }
 }
